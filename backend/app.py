46ed049b4071a20192441e0cfc774ae9e12fb953
jaswant136 <jaswantjasu2006@gmail.com>
2026-05-12 18:14:47 +0530

    fixed render deployment

--- backend/app.py
+++ backend/app.py
@@ -957,9 +957,7 @@ import os
 
 if __name__ == "__main__":
 
-    port = int(os.environ.get("PORT", 5000))
-
     app.run(
         host="0.0.0.0",
-        port=port
+        port=int(os.environ.get("PORT", 10000))
     )
